bf5bbda98543af6a010da2911ae5f77d71abe7ba
omarkadis <omarkadis804@gmail.com>
2024-12-13 11:44:36 -0800

    update and remove all

--- notes/urls.py
+++ notes/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Register, Login, Logout, GetToken, NoteCreate, NoteRead, NoteUpdate, NoteBulkUpdate, NoteDelete, DeleteAllData
+from .views import Register, Login, Logout, GetToken, NoteCreate, NoteRead, NoteReadDetail, NoteUpdate, NoteBulkUpdate, NoteDelete, DeleteAllData
 
 urlpatterns = [
     path('register/', Register.as_view(), name='register'),
@@ -8,8 +8,9 @@ urlpatterns = [
     path('gettoken/', GetToken.as_view(), name='get-token'),
     path('notes/create/', NoteCreate.as_view(), name='note-create'),
     path('notes/', NoteRead.as_view(), name='note-read'),
+    path('notes/<int:noteid>/', NoteReadDetail.as_view(), name='note-read-detail'),
     path('notes/update/<int:noteid>/', NoteUpdate.as_view(), name='note-update'),
-    path('notes/update/', NoteBulkUpdate.as_view(), name='note-update'),
+    path('notes/update/', NoteBulkUpdate.as_view(), name='note-bulk-update'),
     path('notes/delete/<int:noteid>/', NoteDelete.as_view(), name='note-delete'),
-    path('notes/delete/', DeleteAllData.as_view(), name='note-delete'),
+    path('notes/delete/', DeleteAllData.as_view(), name='delete-all-data'),
 ]
